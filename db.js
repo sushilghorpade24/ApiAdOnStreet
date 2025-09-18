0f39acf1d51cefa606f3fd68429d13081e61ebbd
Sushil Ghorpade <ghorpadesushil24@gmail.com>
2025-09-17 22:24:45 -0700

    Setup Railway MySQL connection

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,   // 👈 add this
+  // port: process.env.DB_PORT,   // 👈 add this
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
